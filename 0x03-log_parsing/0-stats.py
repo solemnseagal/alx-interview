976322b02ba65a668579f1719ebdc4fcaeaff315
David Echoda <davidechoda123@gmail.com>
2022-04-29 00:39:17 +0000

    solution to task

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+   A script that reads stdin line by line and computes metrics
+"""
+import sys
+
+
+def print_msg(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, val in sorted(codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+file_size = 0
+code = 0
+line_count = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
+
+        if len(parsed_line) > 2:
+            line_count += 1
+
+            if line_count <= 10:
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
+
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if (line_count == 10):
+                print_msg(codes, file_size)
+                line_count = 0
+
+finally:
+    print_msg(codes, file_size)
